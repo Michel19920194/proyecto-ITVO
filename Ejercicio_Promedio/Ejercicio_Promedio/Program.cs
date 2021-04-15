f5eb4e7b1d42036563c9ca8f49a6b88e4cc89b14
Michel Alexis Jacinto Castillo <82552446+Michel19920194@users.noreply.github.com>
2021-04-14 20:37:54 -0500

    michel alexis jacinto castillo 19920194
    esta es la primera tareas

---
+++ Ejercicio_Promedio/Ejercicio_Promedio/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ejercicio_Promedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Alumno = 0;
+            Console.WriteLine("Ingresar la cantidad de alumnos");
+            Alumno = Convert.ToInt32(Console.ReadLine());
+            decimal[,] arregloCalificaciones = new decimal[Alumno, 2];
+            Console.WriteLine();
+            for (int i = 0; i <= Alumno - 1; i++)
+            {
+                Console.WriteLine("Calificación del alumno #" + (i + 1).ToString());
+                arregloCalificaciones[i, 0] = Convert.ToDecimal(Console.ReadLine());
+                arregloCalificaciones[i, 1] = (i + 1);//Alumno
+                Console.WriteLine();
+            }
+            decimal calificacion = 0;
+            decimal alumno2 = 0;
+            for (int i = 0; i <= Alumno - 1; i++)
+            {
+                if (arregloCalificaciones[i, 0] > calificacion)
+                {
+                    calificacion = arregloCalificaciones[i, 0];
+                    alumno2 = arregloCalificaciones[i, 1] = (i + 1);
+                }
+
+            }
+            Console.WriteLine("El alumno con el mejor promedio es el alumno #{0} con una calificación de:{1}",
+               alumno2.ToString(),
+               calificacion.ToString());
+            Console.WriteLine();
+
+            calificacion = 10;
+            alumno2 = 0;
+            for (int i = 0; i <= Alumno - 1; i++)
+            {
+                if (arregloCalificaciones[i, 0] < calificacion)
+                {
+                    calificacion = arregloCalificaciones[i, 0];
+                    alumno2= arregloCalificaciones[i, 1] = (i + 1);
+                }
+
+            }
+            Console.WriteLine("El alumno con el peor promedio es el alumno #{0} con una calificación de:{1}",
+               alumno2.ToString(),
+               calificacion.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
